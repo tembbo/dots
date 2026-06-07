360e0dbcf4e641ea929cd0b007bc5d833b78871e
tembbo <m@tembbo.com>
2026-06-07 19:10:27 +0200

    fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,68 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	event = { "BufEnter" },
+	branch = "main",
+	event = { "BufReadPost", "BufNewFile" },
 	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })
+		require("nvim-treesitter").update()
 	end,
-	config = function()
-		---@diagnostic disable: missing-fields
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"css",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"vimdoc",
-				"zig",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-		})
+	opts = {
+		ensure_installed = {
+			"bash",
+			"css",
+			"html",
+			"javascript",
+			"typescript",
+			"json",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"vimdoc",
+			"zig",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		},
+	},
+	config = function(_, opts)
+		require("nvim-treesitter").setup(opts)
 	end,
 }
+
+-- return {
+-- 	"nvim-treesitter/nvim-treesitter",
+-- 	branch = "main",
+-- 	event = { "BufEnter" },
+-- 	build = function()
+-- 		require("nvim-treesitter.install").update({ with_sync = true })
+-- 	end,
+-- 	config = function()
+-- 		---@diagnostic disable: missing-fields
+-- 		require("nvim-treesitter.configs").setup({
+-- 			ensure_installed = {
+-- 				"bash",
+-- 				"css",
+-- 				"html",
+-- 				"javascript",
+-- 				"json",
+-- 				"lua",
+-- 				"markdown",
+-- 				"markdown_inline",
+-- 				"svelte",
+-- 				"vimdoc",
+-- 				"zig",
+-- 			},
+-- 			auto_install = true,
+-- 			highlight = {
+-- 				enable = true,
+-- 			},
+-- 			indent = {
+-- 				enable = true,
+-- 			},
+-- 		})
+-- 	end,
+-- }
