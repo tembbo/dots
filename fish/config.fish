fef4b3a00207618410efc7b623d3fd59c9391830
tembbo <m@tembbo.com>
2026-06-11 23:27:28 +0200

    update zig stable version

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,8 @@ set -e fish_user_paths
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.bun/bin
 fish_add_path ~/.local/bin
-fish_add_path ~/.local/bin/zig-aarch64-macos-0.17.0-dev.44+0177cb57c
+fish_add_path ~/.local/bin/zig-aarch64-macos-0.16.0
+# fish_add_path ~/.local/bin/zig-aarch64-macos-0.17.0-dev.44+0177cb57c
 
 set -x GPG_TTY (tty)
 
